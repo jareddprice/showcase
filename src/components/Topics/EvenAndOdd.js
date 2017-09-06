3c47d3b8e2738a0b49200b83e88b4e5b539bb018
Jared Price <jaredprice25@gmail.com>
2017-09-05 20:47:59 -0600

    end of class 9-5

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class EvenAndOdd extends Component {
+
+    constructor () {
+        super();
+
+        this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: ''
+        }
+    }
+    
+    render() {
+        return (
+            <div className="puzzleBox evenAndOddPB">
+                <h4>Evens and Odds</h4>
+                <input className="inputLine"></input>
+                <button className="confirmationButton"></button>
+                <span className="resultsBox"></span>
+                <span className="resultsBox"></span>
+
+                </div>
+        )
+    }
+}
